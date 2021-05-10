fabca13bee1d28f1c4d84d22f320e19ccdd6244f
Raquel <ragontru25@gmail.com>
2021-05-10 10:21:20 +0200

    arreglar tipos de tarjeta y envío

---
+++ src/app/modelo/Tarjeta.ts
@@ -0,0 +1,10 @@
+export class Tarjeta {
+
+    constructor(public numTarjeta: string,
+        public expira: string,
+        public cvv: string,
+        public tipo: string,
+        public clientePromocional: string) {
+
+    }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { NavigationExtras } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { Tarjeta } from '../modelo/Tarjeta';
 
 @Component({
   selector: 'app-tab1',
@@ -10,14 +12,8 @@ import { NavController } from '@ionic/angular';
 export class Tab1Page implements OnInit {
 
   formulario: FormGroup;
+  tipos: Array<string>;
   grupoDatos: FormGroup;
-
-  tipo = [
-    { nombre: "American Express" },
-    { nombre: "Discover" },
-    { nombre: "MasterCard" },
-    { nombre: "Visa" }
-  ];
 
   validation_messages = {
     'numTarjeta': [
@@ -25,24 +21,68 @@ export class Tab1Page implements OnInit {
       { type: 'minlength', message: 'El número de tarjeta debe tener 16 caracteres.' },
       { type: 'maxlength', message: 'El número de tarjeta debe tener 16 caracteres.' },
       { type: 'pattern', message: 'El número de tarjeta sólo contiene números.' }
+    ],
+    'expira': [
+      { type: 'required', message: 'Se requiere la fecha de vencimiento.' }
+    ],
+    'cvv': [
+      { type: 'required', message: 'Se requiere el código de verificación.' }
+    ],
+    'tipo': [
+      { type: 'required', message: 'Se requiere la fecha de vencimiento.' }
+    ],
+    'grupoDatos': [
+      { type: 'required', message: 'Tipo requerido' }
     ]
   }
 
   constructor(
-    // public formBuilder: FormBuilder
-    // private navCtrl: NavController
+    public formBuilder: FormBuilder,
+    private navCtrl: NavController
   ) { }
 
   ngOnInit() {
+    this.tipos = [
+      "American Express",
+      "Discover",
+      "MasterCard",
+      "Visa"
+    ];
     this.grupoDatos = new FormGroup({
       numTarjeta: new FormControl('', Validators.compose([
         Validators.maxLength(16),
         Validators.minLength(16),
-        Validators.pattern('[0-9]'),
+        Validators.pattern('^[0-9]{16}'),
         Validators.required
-      ]))
-    })
+      ])),
+      expira: new FormControl('', Validators.required),
+      cvv: new FormControl('', Validators.required),
+      tipo: new FormControl(this.tipos[0], Validators.required),
+      clientePromocional: new FormControl(false, Validators.pattern('true'))
+    }, (formGroup: FormGroup) => {
+      return this.validarGrupoDatos(formGroup);
+    });
   }
 
+  validarGrupoDatos(fg: FormGroup) {
+    return null;
+  }
+
+  onSubmit(values) {
+    let tarjeta: Tarjeta;
+    tarjeta = new Tarjeta(values['grupoDatos']['numTarjeta'],
+      values['grupoDatos']['expira'],
+      values['grupoDatos']['cvv'],
+      values['grupoDatos']['tipo'],
+      values['grupoDatos']['clientePromocional']);
+
+    console.log(Tarjeta);
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+        tarjeta: JSON.stringify(tarjeta)
+      }
+    };
+    this.navCtrl.navigateForward('/pagina2', navigationExtras);
+  }
 }
 
