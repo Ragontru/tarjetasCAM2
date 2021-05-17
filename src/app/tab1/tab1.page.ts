96ef8c38b621189eec2eeef277ff3c656f4db3da
Raquel <ragontru25@gmail.com>
2021-05-17 10:10:37 +0200

    comparar con la practica 16

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -19,7 +19,6 @@ export class Tab1Page implements OnInit {
     'numTarjeta': [
       { type: 'required', message: 'El número de tarjeta es necesario.' },
       { type: 'minlength', message: 'El número de tarjeta debe tener 16 caracteres.' },
-      { type: 'maxlength', message: 'El número de tarjeta debe tener 16 caracteres.' },
       { type: 'pattern', message: 'El número de tarjeta sólo contiene números.' }
     ],
     'expira': [
@@ -48,11 +47,11 @@ export class Tab1Page implements OnInit {
       "MasterCard",
       "Visa"
     ];
+
     this.grupoDatos = new FormGroup({
       numTarjeta: new FormControl('', Validators.compose([
-        Validators.maxLength(16),
         Validators.minLength(16),
-        Validators.pattern('^[0-9]{16}'),
+        Validators.pattern('^[0-9]*$'),
         Validators.required
       ])),
       expira: new FormControl('', Validators.required),
@@ -62,6 +61,11 @@ export class Tab1Page implements OnInit {
     }, (formGroup: FormGroup) => {
       return this.validarGrupoDatos(formGroup);
     });
+
+    this.formulario = this.formBuilder.group({
+      grupoDatos: this.grupoDatos
+    });
+
   }
 
   validarGrupoDatos(fg: FormGroup) {
@@ -69,6 +73,8 @@ export class Tab1Page implements OnInit {
   }
 
   onSubmit(values) {
+    console.log(values);
+
     let tarjeta: Tarjeta;
     tarjeta = new Tarjeta(values['grupoDatos']['numTarjeta'],
       values['grupoDatos']['expira'],
@@ -76,10 +82,9 @@ export class Tab1Page implements OnInit {
       values['grupoDatos']['tipo'],
       values['grupoDatos']['clientePromocional']);
 
-    console.log(Tarjeta);
     let navigationExtras: NavigationExtras = {
       queryParams: {
-        tarjeta: JSON.stringify(tarjeta)
+        tarjeta: JSON.stringify(values)
       }
     };
     this.navCtrl.navigateForward('/pagina2', navigationExtras);
